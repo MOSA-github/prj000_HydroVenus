09832794315f3f39e286a440aa2eba09f4199fde
yuya-0411 <pzgq5uh7@s.okayama-u.ac.jp>
2024-03-18 19:41:47 +0900

    update

--- stepper_a4988/stepper_a4988_rocking/stepper_a4988_rocking.ino
+++ stepper_a4988/stepper_a4988_rocking/stepper_a4988_rocking.ino
@@ -1,16 +1,18 @@
 #define STEP    32
 #define DIR     33
 #define SENSOR  34
-#define SW      35
-const float RPM_MAX = 90.0;
 
-int sensor, interval, true_speed, pre_time;
+float RPM_MAX = 80.0;
+int sensor, interval=2;
+float angle = 90;
+int steps = angle*2/1.8;
 
 void setup() {
   Serial.begin(115200); 
   pinMode(DIR, OUTPUT);
   pinMode(STEP, OUTPUT);
   pinMode(SENSOR, INPUT);
+
   digitalWrite(DIR, LOW);
   digitalWrite(STEP, LOW);
 }
@@ -18,22 +20,34 @@ void setup() {
 void loop() {
   sensor = analogRead(SENSOR);
   if (sensor > 300){
+  // interval = (int)(3.0/(10.0 * RPM_MAX) * 1000000);
     interval = (int)((3.0/(10.0 * (analogRead(SENSOR)/4095.0) * RPM_MAX)) * 1000000);
     digitalWrite(DIR, HIGH);
     // Serial.printf("FFFFF: %d, RRRRR: %f, RRRRR: %d\n", analogRead(SENSOR), (3.0/(10.0 * (analogRead(SENSOR)/4095.0) * RPM_MAX)) * 1000000, interval);
 
-    // for (int i=0; i<200; i++) {
+    for (int i=0; i<steps; i++) {
       // pre_time = micros();
       digitalWrite(STEP, HIGH);
       delayMicroseconds(interval/2);
       digitalWrite(STEP, LOW);
       delayMicroseconds(interval/2);
       // true_speed = int((3.0/(10.0*float(micros()-pre_time)))*1000000);
+    }
+    delay(500);
+
+    digitalWrite(DIR, LOW);
+    for (int i=0; i<steps; i++) {
+      // pre_time = micros();
+      digitalWrite(STEP, HIGH);
+      delayMicroseconds(interval/2);
+      digitalWrite(STEP, LOW);
+      delayMicroseconds(interval/2);
+      // true_speed = int((3.0/(10.0*float(micros()-pre_time)))*1000000);
+    }
+    delay(500);
   }
 
-  else{
-    digitalWrite(STEP, LOW);
-  }
-
-  // Serial.printf("target_rpm: %d, true_rpm: %d\n", int((analogRead(SENSOR)/4095.0) * RPM_MAX), true_speed); 
+  // else{
+  //   digitalWrite(STEP, LOW);
+  // }
 }
