2424a25c707057cb1bfa82e427ca0df4aef6cc9d
yuya-0411 <pzgq5uh7@s.okayama-u.ac.jp>
2024-03-24 21:51:38 +0900

    update

--- stepper_a4988/stepper_a4988_rocking/stepper_a4988_rocking.ino
+++ stepper_a4988/stepper_a4988_rocking/stepper_a4988_rocking.ino
@@ -1,11 +1,20 @@
+#include <uTimerLib.h>
+
 #define STEP    32
 #define DIR     33
 #define SENSOR  34
 
-float RPM_MAX = 80.0;
-int sensor, interval=2;
-float angle = 90;
+float RPM_MAX = 30.0;
+int sensor;
+float angle = 45;
 int steps = angle*2/1.8;
+
+unsigned long tmr;
+int move_direction = 0;
+int speed = 0;
+float angle_now = 0;
+
+int interval = (int)((3.0/(10.0 * RPM_MAX)) * 1000000);
 
 void setup() {
   Serial.begin(115200); 
@@ -15,39 +24,31 @@ void setup() {
 
   digitalWrite(DIR, LOW);
   digitalWrite(STEP, LOW);
+
+  // タイマ割り込み設定
+  TimerLib.setInterval_us(measure, 500000);
 }
 
 void loop() {
-  sensor = analogRead(SENSOR);
-  if (sensor > 300){
-  // interval = (int)(3.0/(10.0 * RPM_MAX) * 1000000);
-    interval = (int)((3.0/(10.0 * (analogRead(SENSOR)/4095.0) * RPM_MAX)) * 1000000);
-    digitalWrite(DIR, HIGH);
-    // Serial.printf("FFFFF: %d, RRRRR: %f, RRRRR: %d\n", analogRead(SENSOR), (3.0/(10.0 * (analogRead(SENSOR)/4095.0) * RPM_MAX)) * 1000000, interval);
-
-    for (int i=0; i<steps; i++) {
-      // pre_time = micros();
+  if(move_direction)  digitalWrite(DIR, HIGH);
+  else                digitalWrite(DIR, LOW);
+  
+  for (int i=0; i<steps; i++) {
+      tmr = millis();
       digitalWrite(STEP, HIGH);
       delayMicroseconds(interval/2);
       digitalWrite(STEP, LOW);
       delayMicroseconds(interval/2);
-      // true_speed = int((3.0/(10.0*float(micros()-pre_time)))*1000000);
-    }
-    delay(500);
-
-    digitalWrite(DIR, LOW);
-    for (int i=0; i<steps; i++) {
-      // pre_time = micros();
-      digitalWrite(STEP, HIGH);
-      delayMicroseconds(interval/2);
-      digitalWrite(STEP, LOW);
-      delayMicroseconds(interval/2);
-      // true_speed = int((3.0/(10.0*float(micros()-pre_time)))*1000000);
-    }
-    delay(500);
+      if(move_direction)  angle_now+=1.8;
+      else                angle_now-=1.8;
+      speed = 1/200.0 * 1/((millis() - tmr)/1000.0)*60.0;
   }
+  
+  move_direction = ~move_direction;  // 値を反転
+}
 
-  // else{
-  //   digitalWrite(STEP, LOW);
-  // }
+
+ 
+void measure() {
+  printf("speed[RPM] : %d\t angle[deg] : %f\n", speed, angle_now);
 }
